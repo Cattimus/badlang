4d35ad989845894bdc271d821d65cab5f0334d3a
Cattimus <github_commit@cattimus.dev>
2025-07-25 20:21:37 -0700

    write function to get the bounds of the next token

--- src/main.c
+++ src/main.c
@@ -38,13 +38,61 @@ void init_symbols()
 	symbols['-'] = minus;
 }
 
+typedef struct _substr
+{
+	char* str;
+	size_t len;
+	size_t cursor;
+}substr;
+
+typedef struct _token
+{
+	token_type t_type;
+	size_t start;
+	size_t end;
+}token;
+
+//get the bounds of our token
+void get_token(substr* str, token* t)
+{
+	size_t c = str->str[str->cursor];
+	size_t start = str->cursor;
+	size_t end = str->cursor;
+	//pure whitespace isn't a symbol
+	if(symbols[c] == whitespace)
+	{
+		return;
+	}
+
+	//grab all symbols until we encounter whitespace
+	while(symbols[c] != whitespace && symbols[c] != invalid && c < str->len)
+	{
+		end++;
+		c = str->str[end];	
+	}
+
+	//if we hit an invalid character
+	if(symbols[c] == invalid)
+	{
+		return;
+	}
+
+	else if(symbols[c] == whitespace)
+	{
+		str->cursor = end + 1;
+	}
+
+	t->start = start;
+	t->end = end;
+}
+
 int main() 
 {
 	init_symbols();
-	char str[] = {"19 + 21 + 118 + 34 - 43 + 21"};
+	char str[] = {"19 + 21 + 118 + 34 - 43 + 21\n"};
 	for(size_t i = 0; i < strlen(str); i++)
 	{
-		if(symbols[str[i]] == invalid)
+		if(symbols[(size_t)str[i]] == invalid)
 		{
 			fprintf(stderr, "invalid symbol detected: %c\n", str[i]);
 			exit(-1);
