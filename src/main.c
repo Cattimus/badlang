a85116a6c72420c04bcf55996274fb3feacb5148
Cattimus <github_commit@cattimus.dev>
2025-07-28 16:30:29 -0700

    updated for more complex langauge

--- src/main.c
+++ src/main.c
@@ -2,65 +2,53 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef enum _State
+typedef enum
 {
-	invalid = -1,
-	number,
+	invalid,
+	add,
+	subtract,
+	multiply,
+	divide,
+	assign,
 	whitespace,
-	operator,
-}State;
+	separator,
+	identifier,
+	literal,
+	digit,
+	decimal,
+	backslash
+}Token;
 
-State classify(char c)
-{
-	if(c == ' ' || c == '\n' || c == '\t')
-	{
-		return whitespace;
-	}
-	else if(c >= '0' && c <= '9')
-	{
-		return number;
-	}
-	else if(c == '+' || c == '-')
-	{
-		return operator;
-	}
+Token classify[127];
 
-	return invalid;
-}
-
-int transitions[3][3] = {
-	{number,  whitespace, invalid},
-	{number,  whitespace, operator},
-	{invalid, whitespace, invalid}
-};
-
-int is_valid(int transition[3][3], State initial, State accepting, const char* str)
-{
-	State state = initial;
-	size_t len = strlen(str);
-	for(size_t i = 0; i < len; i++)
-	{
-		char c = str[i];
-		int j = classify(c);
-
-		if(j == invalid)
-		{
-			return 0;
-		}
-	
-		state = transition[state][j];
-
-		if(state == invalid)
-		{
-			return 0;
-		}
-	}
-
-	return state == accepting;
-}
 
 int main() 
 {
-	printf("%d\n", is_valid(transitions, number, number, "21 + 14 - 19 + 85 + 45 - 92 + 101"));
-	return 0;
+	classify['\n'] = whitespace;
+	classify['\t'] = whitespace;
+	classify[' '] = whitespace;
+
+	classify['0'] = digit;
+	classify['1'] = digit;
+	classify['2'] = digit;
+	classify['3'] = digit;
+	classify['4'] = digit;
+	classify['5'] = digit;
+	classify['6'] = digit;
+	classify['7'] = digit;
+	classify['8'] = digit;
+	classify['9'] = digit;
+	classify['.'] = decimal;
+
+	//separator
+	classify[';'] = separator;
+
+	//operators
+	classify['*'] = multiply;
+	classify['+'] = add;
+	classify['/'] = divide;
+	classify['-'] = subtract;
+	classify['='] = assign;
+
+	classify['/'] = backslash;
 }
