5ae645c93d3d87f4a446ca017432c896a6b31af0
Cattimus <github_commit@cattimus.dev>
2025-07-27 21:49:07 -0700

    initial concept for state transition

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef enum _State
+{
+	number,
+	whitespace,
+	operator
+}State;
+
 int is_whitespace(char c)
 {
 	return (c == ' ' || c == '\n' || c == '\t');
@@ -58,6 +65,63 @@ int transition_operator(char c)
 	else
 	{
 		return 0;
+	}
+}
+
+//TODO - this won't work because it only goes one direction
+int is_valid(const char* str)
+{
+	size_t len = strlen(str);
+	State state = number;
+	for(size_t i = 0; i < len; i++)
+	{
+		char c = str[i];
+		switch(state)
+		{
+			case number:
+			{
+				int result = transition_number(c);
+				if(result < 0)
+				{
+					return 0;
+				}
+				else if(result == 1)
+				{
+					state += 1;
+				}
+				break;
+			}
+
+
+			case whitespace:
+			{
+				int result = transition_whitespace(c);
+				if(result < 0)
+				{
+					return 0;
+				}
+				else if(result == 1)
+				{
+					state += 1;
+				}
+				break;
+			}
+
+			case operator:
+			{
+				int result = transition_operator(c);
+				if(result < 0)
+				{
+					return 0;
+				}
+				else if(result == 1)
+				{
+					state -= 1;
+				}
+
+				break;
+			}
+		}
 	}
 }
 
