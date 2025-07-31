dd446684e43dcc11799559d58d86a84189957222
Cattimus <github_commit@cattimus.dev>
2025-07-31 14:12:13 -0700

    added lexer function

--- src/lang.py
+++ src/lang.py
@@ -8,7 +8,8 @@ multiline_comment = re.compile("(?s)/\*.*\*/") #comments are ignored by the pars
 #these are necessary for our langauge to compile
 literal = re.compile("\d+\.*\d*")
 identifier = re.compile("[a-zA-z_]\w*")
-operator = re.compile("[+-*/=]")
+
+operators = "+-/*="
 
 class Type(Enum):
 	operator = 1
@@ -21,3 +22,50 @@ class Node:
 	value = None
 
 program = "14 + 21 - 9 + 13"
+
+#first we run our program through a lexer
+def lex(s):
+	symbols = []
+
+	for i in range(0,len(s)):
+
+		#literal (number)
+		if s[i].isnumeric():
+			l = literal.search(s, i)
+			if l != None:
+				l = l.expand()
+				i += len(l)
+				symbols.append((Type.literal, int(l)))
+
+		#search for comments
+		elif s[i] == '/':
+			sl = line_comment.search(s, i)
+			ml = multiline_comment.search(s, i)
+
+			if sl != None:
+				i += len(sl.expand())
+			
+			elif ml != None:
+				i += len(ml.expand())
+		
+		#operator
+		elif s[i] in operators:
+			i += 1
+			symbols.append((Type.operator, l))
+		
+		#identifier
+		elif s[i].isalpha():
+			l = literal.search(s, i)
+			if l != None:
+				l = l.expand()
+				i += len(l)
+				symbols.append((Type.identifier, l))
+
+		#hit unidentified symbol
+		else:
+			return None
+		
+	return symbols
+
+		
+
