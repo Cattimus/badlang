caaaf4df57f9e68efff4d03170ddcc6f2b1776a7
Cattimus <github_commit@cattimus.dev>
2025-07-31 16:50:58 -0700

    add visualization for tree

--- src/lang.py
+++ src/lang.py
@@ -21,6 +21,7 @@ class Node:
 	right = None
 	lex_type = None
 	value = None
+	name = None
 
 #first we run our program through a lexer
 def lex(s):
@@ -70,21 +71,75 @@ def lex(s):
 		
 	return symbols
 
+labels = []
+
 #create visual representation of tree
 def ident_from_node(root):
-	return f'"{root.lex_type}\n{root.value}"'
+	n = f'{root.name}'
+	return n
 
-def tree_to_graph(root, output):
+def tree_to_graph(root):
+	global labels
+	output = ""
+
+	label = root.name + f'[label="{root.value}"]'
+	if not label in labels:
+		labels.append(label)
+
 	if root.left != None:
-		output += ident_from_node(root) + " -- " + ident_from_node(root.left) + "\n"
-		tree_to_graph(root.left, output)
+		output += root.name + " -- " + root.left.name + "\n"
+		output += tree_to_graph(root.left)
 	
 	if root.right != None:
-		output += ident_from_node(root) + " -- " + ident_from_node(root.right) + "\n"
-		tree_to_graph(root.right, output)
+		output += root.name + " -- " + root.right.name + "\n"
+		output += tree_to_graph(root.right)
 
+	return output
+
+def tree_str(root):
+	output = "graph {\n"
+	output += tree_to_graph(root)
+	for label in labels:
+		output += label + "\n"
+	output += "}"
 	return output
 
 # get symbols from program/string input
 program = "14 + l + 21 - 9 + 13"
 symbols = lex(program)
+
+root = Node()
+root.name = 'a'
+cursor = root
+
+current_name = 'b'
+
+for symbol in symbols:
+	c = Node()
+	c.lex_type = symbol[0]
+	c.value = symbol[1]
+	c.name = current_name
+	current_name = chr(ord(current_name)+1)
+
+	if symbol[0] == Type.literal or symbol[0] == Type.identifier:
+		if cursor.left != None and cursor.right == None:
+			cursor.right = c
+			cursor = cursor.right
+		elif cursor.left == None:
+			cursor.left = c
+
+	elif symbol[0] == Type.operator:
+		if cursor.lex_type == None:
+			cursor.lex_type = c.lex_type
+			cursor.value = c.value
+		elif cursor.right == None:
+			cursor.right = c
+			cursor = cursor.right
+
+		#rotate values
+		else:
+			temp = cursor
+			cursor = c
+			cursor.left = temp
+
+print(tree_str(root))
