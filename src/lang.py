a65a4f4b734d88e5e0145708fd2c5dd5383a67c1
Cattimus <github_commit@cattimus.dev>
2025-07-31 13:51:20 -0700

    add enum for symbol types

--- src/lang.py
+++ src/lang.py
@@ -1,11 +1,19 @@
 import re
+from enum import Enum
 
 #regexes we'll need to parse our language
 line_comment = re.compile("//.*")
-multiline_comment = re.compile("(?s)/\*.*\*/")
+multiline_comment = re.compile("(?s)/\*.*\*/") #comments are ignored by the parser
+
+#these are necessary for our langauge to compile
 literal = re.compile("\d+\.*\d*")
 identifier = re.compile("[a-zA-z_]\w*")
 operator = re.compile("[+-*/=]")
+
+class Type(Enum):
+	operator = 1
+	literal = 2
+	identifier = 3
 
 class Node:
 	left = None
