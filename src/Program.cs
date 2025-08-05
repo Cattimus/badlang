f56a793ad996b14a241d36d2a6d84dd91b3c7c75
Cattimus <github_commit@cattimus.dev>
2025-08-04 20:11:16 -0700

    single characters handled

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,12 @@
-﻿enum Token_Type
+﻿using System.Text.RegularExpressions;
+
+enum Token_Type
 {
 	invalid = -1,
-	openbracket,
-	closebracket,
+	openBracket,
+	closeBracket,
+	openSquareBracket,
+	closeSquareBracket,
 	separator,
 	identifier,
 	literal,
@@ -21,13 +25,55 @@ class Token
 		t = Token_Type.invalid;
 		raw_data = "";
 	}
+
+	public Token(Token_Type t, string raw_data)
+	{
+		this.t = t;
+		this.raw_data = raw_data;
+	}
 };
 
 static class Lexer
 {
-	static List<Token> process(ref string input)
+	public static List<Token> process(ref string input)
 	{
 		List<Token> l = new List<Token>();
-		return l;
-	}
-};
+
+		foreach (char c in input)
+		{
+			switch (c)
+			{
+				case ':':
+				case ',':
+					{
+						l.Add(new Token(Token_Type.separator, c.ToString()));
+						break;
+					}
+				case '{':
+					{
+						l.Add(new Token(Token_Type.openBracket, c.ToString()));
+						break;
+					}
+				case '}':
+					{
+						l.Add(new Token(Token_Type.closeBracket, c.ToString()));
+						break;
+					}
+				case '[':
+					{
+						l.Add(new Token(Token_Type.openSquareBracket, c.ToString()));
+						break;
+					}
+				case ']':
+					{
+						l.Add(new Token(Token_Type.closeSquareBracket, c.ToString()));
+						break;
+					}
+
+				
+			}
+		}
+
+			return l;
+		}
+	};
