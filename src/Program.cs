79f2efc0f19bba9a81138a9e0409a163a3236aea
Cattimus <github_commit@cattimus.dev>
2025-08-04 20:01:09 -0700

    getting things set up

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿enum Token_Type
+{
+	invalid = -1,
+	openbracket,
+	closebracket,
+	separator,
+	identifier,
+	literal,
+	obj,
+	arr
+};
+
+//token for lexer prior to parsing
+class Token
+{
+	Token_Type t;
+	string raw_data;
+
+	public Token()
+	{
+		t = Token_Type.invalid;
+		raw_data = "";
+	}
+};
+
+static class Lexer
+{
+	static List<Token> process(ref string input)
+	{
+		List<Token> l = new List<Token>();
+		return l;
+	}
+};
